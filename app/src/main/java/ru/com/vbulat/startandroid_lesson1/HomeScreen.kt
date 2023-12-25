192945d0ff4f19bab2d1f31c95ad147f345ae77d
VB <Vladimir.Bulat@gmail.com>
2023-12-25 11:39:24 +0300

    Lesson 3

--- app/src/main/java/ru/com/vbulat/startandroid_lesson1/HomeScreen.kt
+++ app/src/main/java/ru/com/vbulat/startandroid_lesson1/HomeScreen.kt
@@ -1,37 +1,31 @@
 package ru.com.vbulat.startandroid_lesson1
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.interaction.DragInteraction
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Start
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
-    Column(
-        modifier = Modifier
-            .background(Color.LightGray)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom,
-    ) {
-        Text(
-            text = "Title",
-            fontSize = 32.sp,
-        )
-        Text(
-            text = "Description",
-            fontSize = 20.sp,
-            modifier = Modifier.align(Start)
-        )
+    Row(verticalAlignment = CenterVertically) {
+        Box {
+            Text("N", fontSize = 48.sp)
+            Text("ame", modifier = Modifier.align(BottomCenter))
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text("Title")
+            Text("Description")
+        }
     }
 }
 

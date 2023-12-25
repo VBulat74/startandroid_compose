bdac683632d1fc7f8463e9b2c7e7e8fb04f590ba
VB <Vladimir.Bulat@gmail.com>
2023-12-25 10:05:07 +0300

    Lesson 1

---
+++ app/src/main/java/ru/com/vbulat/startandroid_lesson1/HomeScreen.kt
@@ -0,0 +1,10 @@
+package ru.com.vbulat.startandroid_lesson1
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(){
+    Text(text = "Home screen")
+    Text(text = "123456789")
+}

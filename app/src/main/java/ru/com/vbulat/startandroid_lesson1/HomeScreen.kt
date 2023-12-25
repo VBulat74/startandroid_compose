2a4c14c83c5c133a6f0d3a8b4037902118434998
VB <Vladimir.Bulat@gmail.com>
2023-12-25 10:47:43 +0300

    Lesson 2

--- app/src/main/java/ru/com/vbulat/startandroid_lesson1/HomeScreen.kt
+++ app/src/main/java/ru/com/vbulat/startandroid_lesson1/HomeScreen.kt
@@ -2,9 +2,24 @@ package ru.com.vbulat.startandroid_lesson1
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(){
-    Text(text = "Home screen")
-    Text(text = "123456789")
+    Text(
+        text = "Home screen",
+        fontSize = 32.sp,
+        color = Color.Green
+    )
+}
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen()
 }

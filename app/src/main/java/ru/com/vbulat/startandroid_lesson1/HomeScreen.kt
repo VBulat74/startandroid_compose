202a7347d98744bc606f23b86a66327a0b7b0cfd
VB <Vladimir.Bulat@gmail.com>
2023-12-25 10:55:24 +0300

    Lesson 3

--- app/src/main/java/ru/com/vbulat/startandroid_lesson1/HomeScreen.kt
+++ app/src/main/java/ru/com/vbulat/startandroid_lesson1/HomeScreen.kt
@@ -1,9 +1,16 @@
 package ru.com.vbulat.startandroid_lesson1
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -11,7 +18,13 @@ fun HomeScreen(){
     Text(
         text = "Home screen",
         fontSize = 32.sp,
-        color = Color.Green
+        color = Color.White,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(Color.Black)
+            .fillMaxSize()
     )
 }
 

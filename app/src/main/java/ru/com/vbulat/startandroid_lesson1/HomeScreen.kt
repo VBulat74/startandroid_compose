3a3f29b8b03011ab88385ea00ad45e2bc04c78a9
VB <Vladimir.Bulat@gmail.com>
2023-12-25 11:26:19 +0300

    Lesson 3 Test Column

--- app/src/main/java/ru/com/vbulat/startandroid_lesson1/HomeScreen.kt
+++ app/src/main/java/ru/com/vbulat/startandroid_lesson1/HomeScreen.kt
@@ -1,31 +1,38 @@
 package ru.com.vbulat.startandroid_lesson1
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.DragInteraction
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun HomeScreen(){
-    Text(
-        text = "Home screen",
-        fontSize = 32.sp,
-        color = Color.White,
-        maxLines = 1,
-        overflow = TextOverflow.Ellipsis,
-        textAlign = TextAlign.Center,
+fun HomeScreen() {
+    Column(
         modifier = Modifier
-            .background(Color.Black)
-            .fillMaxSize()
-    )
+            .background(Color.LightGray)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom,
+    ) {
+        Text(
+            text = "Title",
+            fontSize = 32.sp,
+        )
+        Text(
+            text = "Description",
+            fontSize = 20.sp,
+            modifier = Modifier.align(Start)
+        )
+    }
 }
 
 @Preview(
@@ -33,6 +40,6 @@ fun HomeScreen(){
     showBackground = true
 )
 @Composable
-fun HomeScreenPreview(){
+fun HomeScreenPreview() {
     HomeScreen()
 }
